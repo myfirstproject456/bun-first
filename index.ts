9169d1f7e0096a63fcb97d86a96e6d56bcf8484d
Deepak Chauhan <dchdeepak@gmail.com>
2026-02-23 04:31:09 +0530

    bit explore

--- index.ts
+++ index.ts
@@ -1,1 +1,56 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+import index from "./index.html";
+process.env.BUN_CONFIG_VERBOSE_FETCH = "true";
+
+const server = Bun.serve({
+  port: 3000,
+
+  fetch(req) {
+    const url = new URL(req.url);
+
+    // /
+    if (url.pathname === "/") {
+      return new Response("Home");
+    }
+
+    // /todo
+    if (url.pathname === "/todo") {
+      const todoLists = [
+        { title: "my first title" },
+        { title: "my second title" },
+      ];
+
+      return new Response(JSON.stringify(todoLists), {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    // /fetch/1
+    if (url.pathname.startsWith("/fetch")) {
+      const id: string = url.pathname.split("/")[2] || "1";
+      return fetchPostById(id);
+    }
+
+    // /figlet
+    if (url.pathname === "/figlet") {
+      const body = figlet.textSync("Bun!");
+      return new Response(body, {
+        headers: { "Content-Type": "text/plain" },
+      });
+    }
+
+    return new Response("Not Found", { status: 404 });
+  },
+});
+
+async function fetchPostById(id: string) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const data = await res.json();
+
+  return new Response(JSON.stringify(data), {
+    headers: { "Content-Type": "application/json" },
+  });
+}
+
+console.log("Server running on http://localhost:3000");
+console.log(`Listening on ${server.url}`);
